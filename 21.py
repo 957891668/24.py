49d5947584d40050091f250803f03a6f36c57343
dubu <957891668@qq.com>
2020-02-27 16:56:14 +0800

    21.py

--- 21.py
+++ 21.py
@@ -1,3 +1,11 @@
 import random
-
+import time
 print("游戏开始！")
+time.sleep(0.5)
+
+def fapai():
+    for i in range(8):
+        print("\r正在发牌."+'.'*(i%4)+'   ',end = "")
+        time.sleep(0.4)
+    print("\r您的牌为：",random.randint(1,13))
+fapai()
